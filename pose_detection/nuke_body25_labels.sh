5227547d2b37baf2dde11ada2d4516b035af2b26
benjamin@titan <abruzzo2@gmail.com>
2019-05-21 15:04:36 -0400

    added label cleanup to remove text files with duplicate images

---
+++ pose_detection/nuke_body25_labels.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/benjamin/datasets/FiveClassThreats/body_25/high/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_25/high/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_25/medium/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_25/medium/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_25/mild/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_25/mild/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_25/low/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_25/low/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_25/zero/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_25/zero/skeletons && rm *
+
+cd /home/benjamin/threat_detection/pose_detection
---
+++ pose_detection/nuke_mpii_labels.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/high/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/high/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/medium/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/medium/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/mild/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/mild/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/low/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/low/skeletons && rm *
+
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/zero/images && rm *
+cd /home/benjamin/datasets/FiveClassThreats/body_mpii/zero/skeletons && rm *
+
+cd /home/benjamin/threat_detection/pose_detection
